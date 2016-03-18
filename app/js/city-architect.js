30119d9cdb0b66f4e229a0233509b59309f86b6a
mpi <mpi.michal.piotrkowski@gmail.com>
2016-03-19 00:45:47 +0100

    dirty implementation of builing placement

--- app/js/city-architect.js
+++ app/js/city-architect.js
@@ -1,26 +1,23 @@
-define(['vendor/three', 'city'], function (THREE, City) {
+define(['vendor/three', 'vendor/underscore', 'city'], function (THREE, _, City) {
 
   return {
     buildCity: buildCity
   };
 
-  function buildCity( /*data*/ ) {
+  function buildCity(data) {
 
     var camera, renderer;
 
     var city = new City();
-    city.addBuilding({
-      x: 0,
-      y: 0,
-      foundations: 200,
-      height: 400
+    _(layout(data)).each(function(b){
+      city.addBuilding(b);
     });
 
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
 
     camera.position.z = 1000;
     camera.position.x = 500;
-    camera.position.y = 500;
+    camera.position.y = 1000;
     camera.lookAt(new THREE.Vector3(0, 0, 0));
 
     renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -46,6 +43,101 @@ define(['vendor/three', 'city'], function (THREE, City) {
       renderer.render(city, camera);
 
     }
+  }
+  
+  function layout(data){
+    
+    var layout = [];
+    var point = {x: 0, y: 0};
+    var directions = [left, up, right, down];
+    var direction = left;
+    var i = 0;
+    
+    data = _(data).sortBy(function(b){ return -b.foundations * b.height; });
+    for(i=0; i<data.length; i++){
+
+      b = data[i];
+      _.extend(b, point);
+      layout.push(b);
+
+      if(i < data.length - 1){
+        point = findNextValidPoint(i, i + 1);
+      }
+      
+      function findNextValidPoint(branch, i){
+        var n = data[i];
+
+        var delta = (data[branch].foundations/2 + n.foundations/2) * 1.6;
+
+        var nextDirection = turnForward(direction);
+        var prevDirection = turnBackward(direction);
+        var sameDirection = direction;
+
+        if(isValidLocationFor(n, nextDirection(point, delta))){
+          direction = nextDirection;
+          return direction(point, delta);
+        } else if(isValidLocationFor(n, sameDirection(point, delta))){
+          direction = sameDirection;
+          return direction(point, delta);
+        } else if(isValidLocationFor(n, prevDirection(point, delta))){
+          direction = prevDirection;
+          return direction(point, delta);
+        } else {
+          return findNextValidPoint(branch-1, i);
+        }
+      }
+    }
+    
+    return layout;
+    
+    function turnForward(direction){
+      return directions[(directions.indexOf(direction) + 1) % 4];
+    }
+    function turnBackward(direction){
+      return directions[(directions.indexOf(direction) + 3) % 4];
+    }
+    
+    function left(p, delta){
+      return {x : p.x - delta, y: p.y};
+    }
+    function right(p, delta){
+      return {x : p.x + delta, y: p.y};
+    }
+    function up(p, delta){
+      return {x : p.x, y: p.y + delta};
+    }
+    function down(p, delta){
+      return {x : p.x, y: p.y - delta};
+    }
+    
+    function isValidLocationFor(building, point){
+      
+      return !_(layout).find(colliding);
+      
+      function colliding(b){
+        var a = {x: point.x, y: point.y, foundations: building.foundations};
+        return _(cornersOf(b)).find(inside(a)) || _(cornersOf(a)).find(inside(b));
+      }
+
+      function cornersOf(b){
+        return [
+          { x: b.x - b.foundations/2, y: b.y - b.foundations/2},
+          { x: b.x - b.foundations/2, y: b.y + b.foundations/2},
+          { x: b.x + b.foundations/2, y: b.y - b.foundations/2},
+          { x: b.x + b.foundations/2, y: b.y + b.foundations/2}
+        ];
+      }
+      
+      function inside(a){
+        
+        return function(point){
+          return (point.x < a.x + a.foundations/2 && point.x > a.x - a.foundations/2) &&
+                (point.y < a.y + a.foundations/2 && point.y > a.y - a.foundations/2);
+        }
+        
+      }
+    }
+    
   }
 
 });
--- app/js/city.js
+++ app/js/city.js
@@ -6,23 +6,23 @@ define(['vendor/three'], function (THREE) {
   function city() {
 
     var scene = new THREE.Scene();
-    
-      var geometry, material, mesh;
-      
-      geometry = new THREE.BoxGeometry(2000, 10, 2000);
-      material = new THREE.MeshLambertMaterial({
-        color: 0xffffff
-      });
 
-      floor = new THREE.Mesh(geometry, material);
-      floor.receiveShadow = true;
-      floor.translateY(-10);
-      scene.add(floor);
+    var geometry, material;
+
+    geometry = new THREE.BoxGeometry(2000, 10, 2000);
+    material = new THREE.MeshLambertMaterial({
+      color: 0xffffff
+    });
+
+    var floor = new THREE.Mesh(geometry, material);
+    floor.receiveShadow = true;
+    floor.translateY(-10);
+    scene.add(floor);
 
     scene.addBuilding = function (building) {
-      
+
       var geometry, material, mesh;
-      
+
       geometry = new THREE.BoxGeometry(building.foundations, building.height, building.foundations);
       material = new THREE.MeshLambertMaterial({
         color: 0xff0000
@@ -31,6 +31,8 @@ define(['vendor/three'], function (THREE) {
       mesh = new THREE.Mesh(geometry, material);
       mesh.receiveShadow = true;
       mesh.translateY(building.height / 2);
+      mesh.translateX(building.x);
+      mesh.translateZ(building.y);
       scene.add(mesh);
     };
 
