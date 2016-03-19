d8cbb16b54da614720e310f72d0fe7d77dc11230
Artur Chyży <artur.chyzy@gmail.com>
2016-03-19 10:04:07 +0100

    Camera when rotating

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,25 @@
 requirejs.config({
-  baseUrl: 'js',
-  urlArgs: 'ts=' + (new Date()).getTime(),
-  shim: {
-    'vendor/three': {
-      exports: 'THREE'
-    },
-    'vendor/FlyControls': {
-      deps: ['vendor/three'],
-      exports: 'THREE.FlyControls'
-    },
-    'vendor/underscore': {
-      exports: '_'
-    },
-    'vendor/jquery': {
-      exports: '$'
+    baseUrl : 'js',
+    urlArgs : 'ts=' + (new Date()).getTime(),
+    shim : {
+        'vendor/three' : {
+            exports : 'THREE'
+        },
+        'vendor/TrackballControls' : {
+            deps : ['vendor/three'],
+            exports : 'THREE.TrackballControls'
+        },
+        'vendor/underscore' : {
+            exports : '_'
+        },
+        'vendor/jquery' : {
+            exports : '$'
+        }
     }
-  }
 });
 
 require(['city-architect', 'vendor/jquery', 'vendor/underscore'], function (cityArchitect, $, _) {
-  
+
 //  var data = [
 //    {
 //      label: 'com.bla.BlaBla',
@@ -30,16 +30,16 @@ require(['city-architect', 'vendor/jquery', 'vendor/underscore'], function (city
 //    }
 //  ];
 
-  $.get('http://source-city.herokuapp.com/api/metrics/aHR0cHM6Ly9naXRodWIuY29tL3NwcmluZy1wcm9qZWN0cy9zcHJpbmctYm9vdC5naXQ=')
-    .done(function(data){
-      var cityData = _(data.fileMetrics).map(function (metric) {
-        return {
-          foundations : metric.dependencies * 5,
-          height : metric.loc,
-          label: metric.label
-        };
-      });
-      cityArchitect.buildCity(cityData);
-    });
+    $.get('http://source-city.herokuapp.com/api/metrics/aHR0cHM6Ly9naXRodWIuY29tL3NwcmluZy1wcm9qZWN0cy9zcHJpbmctYm9vdC5naXQ=')
+        .done(function (data) {
+            var cityData = _(data.fileMetrics).map(function (metric) {
+                return {
+                    foundations : metric.dependencies * 5,
+                    height : metric.loc,
+                    label : metric.label
+                };
+            });
+            cityArchitect.buildCity(cityData);
+        });
 
 });
