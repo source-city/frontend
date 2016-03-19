26ad794942045f68e9b8bc1e679ea2922c3529f4
Artur Chyży <artur.chyzy@gmail.com>
2016-03-19 10:46:50 +0100

    Fixes

--- app/js/city-list.js
+++ app/js/city-list.js
@@ -1,33 +1,33 @@
 define(['vendor/jquery', 'vendor/underscore', 'vendor/virtual-dom'], function($, _, virtualDom){
-  
+
   var createElement = virtualDom.create;
   var h = virtualDom.h;
   var diff = virtualDom.diff;
   var patch = virtualDom.patch;
-  
+
   var cityList = [];
   var tree, element;
-  
+
   $('.showNewCity').click(function(){
     $('#newCityModal').show().toggleClass('fade');
   });
 
-  $('.closeCityModal').click(function(){
-    $('#newCityModal').toggleClass('fade').hide();
-  })
-
-  $('.buildCity').click(function(){
-    $.post({
-      url: 'http://source-city.herokuapp.com/api/repositories',
-      contentType: 'application/json',
-      data: JSON.stringify({
-        url: $('input.url').val(),
-        name: $('input.name').val()
-      })
-    }).done(function(){
-      $('#newCityModal').toggleClass('fade').hide();
+    $('.closeCityModal').click(function () {
+        $('#newCityModal').toggleClass('fade').hide();
     });
-  })
+
+    $('.buildCity').click(function () {
+        $.post({
+            url : 'http://source-city.herokuapp.com/api/repositories',
+            contentType : 'application/json',
+            data : JSON.stringify({
+                url : $('input.url').val(),
+                name : $('input.name').val()
+            })
+        }).done(function () {
+            $('#newCityModal').toggleClass('fade').hide();
+        });
+    });
 
   $.get('http://source-city.herokuapp.com/api/repositories')
     .done(function(data){
@@ -35,7 +35,7 @@ define(['vendor/jquery', 'vendor/underscore', 'vendor/virtual-dom'], function($,
       update(cityList);
       updates();
     });
-  
+
   function updates(){
     var events = new EventSource('http://source-city.herokuapp.com/api/updates');
     events.onmessage = function(ev){
@@ -49,13 +49,13 @@ define(['vendor/jquery', 'vendor/underscore', 'vendor/virtual-dom'], function($,
         cityList.push(progress);
       }
       update(cityList);
-    }
+    };
   }
-  
+
   function update(data){
-    
+
       var newtree = render(data);
-    
+
       if(!tree){
         tree = newtree;
         element = createElement(tree);
@@ -65,13 +65,13 @@ define(['vendor/jquery', 'vendor/underscore', 'vendor/virtual-dom'], function($,
         tree = newtree;
       }
   }
-  
+
   function render(data){
 
     return h('div.list-group',
       $.map(data, city)
     );
-    
+
     function city(city){
       return h('a.list-group-item', [
         h('div.row', [
@@ -80,7 +80,7 @@ define(['vendor/jquery', 'vendor/underscore', 'vendor/virtual-dom'], function($,
           ((city.processed && city.processed !== city.total) ? progress() : actions())
         ])
       ]);
-      
+
       function label(){
         return h('div.col-md-5', [
           h('h4.list-group-item-heading', [ city.name ]),
@@ -88,20 +88,20 @@ define(['vendor/jquery', 'vendor/underscore', 'vendor/virtual-dom'], function($,
         ]);
       }
       function ready(){
-        
+
         var status = (city.ready ? 'READY' : 'PENDING');
         if(city.processed){
           status = ((city.processed === city.total) ? 'READY' : 'IN CONSTRUCTION');
         }
-        
+
         return h('div.col-md-3', [
           h('h4.list-group-item-heading.status', [status])
         ]);
       }
       function progress(){
-        
+
         var percentage = (city.processed * 100 / city.total).toFixed(0);
-        
+
         return h('div.col-md-4.actions', [
           h('div.progress', [
             h('div.progress-bar.progress-bar-striped.active', {style: {width: percentage + '%'}})
@@ -117,8 +117,8 @@ define(['vendor/jquery', 'vendor/underscore', 'vendor/virtual-dom'], function($,
         ]);
       }
     }
-    
+
   }
-  
-  
+
+
 });
